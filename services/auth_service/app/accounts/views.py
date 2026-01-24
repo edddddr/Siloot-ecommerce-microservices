ba2d6c1486322ad104cc0aaf35fdf2c34a3f0cdd
edddddr <endt6342@gmail.com>
2026-01-23 17:25:25 -0900

    feat:auth-service Add new user authentication service

---
+++ services/auth_service/app/accounts/views.py
@@ -0,0 +1,17 @@
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import RegisterSerializer
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+class MeView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        return Response({
+            "id": request.user.id,
+            "email": request.user.email
+        })
