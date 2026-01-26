27962ca82d33fa2ae265640147c7c4af8fd30223
edddddr <endt6342@gmail.com>
2026-01-26 02:54:44 -0900

    feat: Synchronous Internal API Call for the services -> cart & order

--- services/order_service/app/core/settings.py
+++ services/order_service/app/core/settings.py
@@ -111,3 +111,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+CART_SERVICE_URL = "http://cart_service:8000"
--- services/order_service/app/orders/views.py
+++ services/order_service/app/orders/views.py
@@ -1,5 +1,9 @@
-from rest_framework import viewsets, permissions
+
+from rest_framework import viewsets, permissions, status
 from .models import Order
+from rest_framework.response import Response
+from orders.services.cart_client import CartServiceClient
+from orders.services.order_creator import create_order_from_cart
 from .serializers import OrderSerializer
 
 
@@ -8,8 +12,20 @@ class OrderViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        # user sees ONLY their orders
         return Order.objects.filter(user_id=self.request.user.id)
 
-    def perform_create(self, serializer):
-        serializer.save(user_id=self.request.user.id)
+    def create(self, request, *args, **kwargs):
+        token = request.auth
+        cart_client = CartServiceClient(token=str(token))
+
+        cart_data = cart_client.get_cart()
+
+        order = create_order_from_cart(
+            user_id=request.user.id,
+            cart_data=cart_data
+        )
+
+        cart_client.clear_cart()
+
+        serializer = self.get_serializer(order)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
