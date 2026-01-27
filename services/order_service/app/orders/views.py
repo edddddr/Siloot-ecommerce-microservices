d50575e539b3c21088bf3ebc4e7e2e7aaf9342f3
edddddr <endt6342@gmail.com>
2026-01-26 20:12:21 -0900

    Confi: auth_service

--- services/order_service/app/orders/views.py
+++ services/order_service/app/orders/views.py
@@ -1,11 +1,11 @@
 
 from rest_framework import viewsets, permissions, status
-from .models import Order
+from rest_framework.decorators import action 
 from rest_framework.response import Response
+from .models import Order
+from .serializers import OrderSerializer
 from orders.services.cart_client import CartServiceClient
 from orders.services.order_creator import create_order_from_cart
-from .serializers import OrderSerializer
-
 
 class OrderViewSet(viewsets.ModelViewSet):
     serializer_class = OrderSerializer
@@ -29,3 +29,10 @@ class OrderViewSet(viewsets.ModelViewSet):
 
         serializer = self.get_serializer(order)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    @action(detail=True, methods=["post"])
+    def mark_paid(self, request, pk=None):
+        order = self.get_object()
+        order.status = "PAID"
+        order.save()
+        return Response({"status": "PAID"}, status=status.HTTP_200_OK)
