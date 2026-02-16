4db65a36fd5fc8896994779583a7d0428c075bc2
edddddr <endt6342@gmail.com>
2026-02-15 17:06:45 -0900

    feat: implement Redis caching for Product service

--- services/product/catalog/views.py
+++ services/product/catalog/views.py
@@ -1,5 +1,8 @@
 from rest_framework import viewsets, filters
+from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
+from django.core.cache import cache
+
 
 from .models import Product, Category
 from .serializers import ProductSerializer, CategorySerializer
@@ -19,3 +22,40 @@ class ProductViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ["category"]
     search_fields = ["name"]
+
+    def list(self, request, *args, **kwargs):
+        query_params = request.query_params.urlencode()
+        cache_key = f"product_list_{query_params}" if query_params else "product_list_all"
+
+        print(f"DEBUG: Looking for Key: {cache_key}")
+        data = cache.get(cache_key)
+
+        if data:
+            print(" - -- -- --   -CACHE HIT -- -- -- -")
+        else:
+            print("- - - - - CACHE MISS - -- - -")
+
+        if not data:
+            queryset = self.filter_queryset(self.get_queryset())
+            serializer = self.get_serializer(queryset, many=True)
+            data = serializer.data
+            cache.set(cache_key, data, timeout=60 * 5)
+        
+        
+
+        return Response(data)
+
+    def clear_product_cache(self):
+        cache.delete_pattern("product_list_*")
+
+    def perform_create(self, serializer):
+        serializer.save()
+        self.clear_product_cache()
+
+    def perform_update(self, serializer):
+        serializer.save()
+        self.clear_product_cache()
+
+    def perform_destroy(self, instance):
+        instance.delete()
+        self.clear_product_cache()
--- services/product/config/settings.py
+++ services/product/config/settings.py
@@ -111,6 +111,15 @@ REST_FRAMEWORK = {
     ),
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
 
 
 
