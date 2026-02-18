61e68070eb143cb115d44507774ccee72a0a52f3
edddddr <endt6342@gmail.com>
2026-02-17 18:40:50 -0900

    feat(product-ai): integrate scikit-learn for vector similarity scoring

--- services/product/config/settings.py
+++ services/product/config/settings.py
@@ -10,7 +10,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)b32ghkgqrq^k%t@x)jv#d%+^kzswpal!#2ko2t&5rejbs&z*_'
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+
+ORDER_SERVICE_URL = os.getenv("ORDER_SERVICE_URL")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
