38b8cb4eba5a1d91c168a273b68dd03a5acb4805
edddddr <endt6342@gmail.com>
2026-02-21 23:01:34 -0900

    fix: communication between tow services calling wiht name of service conainer

--- services/cart/carts/permissions.py
+++ services/cart/carts/permissions.py
@@ -6,16 +6,24 @@ class IsAuthenticatedViaAuthService(BasePermission):
 
     def has_permission(self, request, view):
         auth_header = request.headers.get("Authorization")
+        print(auth_header, '\n', settings.AUTH_SERVICE_URL)
 
         if not auth_header:
             return False
 
+        # response = requests.get("http://127.0.0.1:8000/api/v1/auth/me/",
+        #     headers={"Authorization": auth_header}
+
+        # )
+
         response = requests.get(
-            f"{settings.AUTH_SERVICE_URL}/profile/me",
-            headers={"Authorization": auth_header}
+            f"{settings.AUTH_SERVICE_URL}/auth/me/",
+            headers={"Authorization": auth_header},
+            timeout=2
         )
 
         if response.status_code != 200:
+            print("Auth service response: ", response.text)
             return False
 
         request.user_data = response.json()
