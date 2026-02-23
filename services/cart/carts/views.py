7a3b8fc3cb37c3aa0a0ab17f91a1279153ba8bb1
edddddr <endt6342@gmail.com>
2026-02-22 16:46:56 -0900

    fix: redudant service layer

--- services/cart/carts/views.py
+++ services/cart/carts/views.py
@@ -1,9 +1,8 @@
-import requests
-from django.conf import settings
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from .permissions import IsAuthenticatedViaAuthService
 from .services import add_item, get_cart, remove_item, clear_cart
+
 
 @api_view(["GET"])
 @permission_classes([IsAuthenticatedViaAuthService])
@@ -15,37 +14,15 @@ def view_cart(request):
 @api_view(["POST"])
 @permission_classes([IsAuthenticatedViaAuthService])
 def add_to_cart(request):
-    product_id = request.data.get("product_id")
-    quantity = request.data.get("quantity")
+    user_id = request.user_data["id"]
 
-    # 1️⃣ Call Inventory Service
-    try:
-        inventory_response = requests.get(
-            f"{settings.INVENTORY_SERVICE_URL}/check/",
-            params={"product_id": product_id, "quantity": quantity},
-            timeout=2
-        )
-    except requests.exceptions.RequestException:
-        return Response(
-            {"error": "Stock verification unavailable"},
-            status=503
-        )
+    cart = add_item(
+        user_id,
+        request.data["product_id"],
+        request.data["quantity"]
+    )
 
-    if inventory_response.status_code != 200:
-        return Response({"error": "Inventory error"}, status=400)
-
-    inventory_data = inventory_response.json()
-
-    if not inventory_data["available"]:
-        return Response(
-            {"error": "Not enough stock"},
-            status=400
-        )
-
-    # 2️⃣ Save to Cart DB
-    # CartItem.objects.create(...)
-
-    return Response({"message": "Item added to cart"})
+    return Response(cart)
 
 
 @api_view(["DELETE"])
