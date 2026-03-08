0128da476cc46e0ed919c9ec864a36f65eefb560
edddddr <endt6342@gmail.com>
2026-03-08 02:01:04 -0900

    feat: business logic

---
+++ cart-service/app/cart/services.py
@@ -0,0 +1,65 @@
+import uuid
+from django.db import transaction
+from .models import Cart, CartItem
+
+
+class CartService:
+
+    @staticmethod
+    def get_or_create_cart(user_id):
+
+        cart, created = Cart.objects.get_or_create(
+            user_id=user_id
+        )
+
+        return cart
+    
+
+    @staticmethod
+    @transaction.atomic
+    def add_item(user_id, product_id, quantity):
+
+        cart = CartService.get_or_create_cart(user_id)
+
+        item, created = CartItem.objects.get_or_create(
+            cart=cart,
+            product_id=product_id,
+            defaults={"quantity": quantity}
+        )
+
+        if not created:
+            item.quantity += quantity
+            item.save()
+
+        return item
+
+
+
+    @staticmethod
+    @transaction.atomic
+    def update_item(cart_item_id, quantity):
+
+        item = CartItem.objects.get(id=cart_item_id)
+
+        item.quantity = quantity
+        item.save()
+
+        return item
+
+
+    @staticmethod
+    @transaction.atomic
+    def remove_item(cart_item_id):
+
+        item = CartItem.objects.get(id=cart_item_id)
+
+        item.delete()
+
+
+    @staticmethod
+    @transaction.atomic
+    def clear_cart(user_id):
+
+        cart = Cart.objects.get(user_id=user_id)
+
+        CartItem.objects.filter(cart=cart).delete()
