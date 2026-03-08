f9dfcc7523e28c013f2681a991c532dcbb0eed4f
edddddr <endt6342@gmail.com>
2026-03-08 00:46:13 -0900

    edit: folder base dev prod environment

--- cart-service/app/config/settings/base.py
+++ cart-service/app/config/settings/base.py
@@ -7,14 +7,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+ 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
