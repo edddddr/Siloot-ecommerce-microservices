af102e3edb21242b74e7698ddab16e7fe6b6fd88
edddddr <endt6342@gmail.com>
2026-03-08 01:34:11 -0900

    feat: admin dashoboard

--- cart-service/app/cart/admin.py
+++ cart-service/app/cart/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Cart, CartItem
 
-# Register your models here.
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ("id", "user_id", "created_at")
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ("id", "cart", "product_id", "quantity")
