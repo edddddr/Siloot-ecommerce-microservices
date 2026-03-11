7297c6ab478eab39487264c70218bb4f6b8284a1
edddddr <endt6342@gmail.com>
2026-03-11 10:32:30 -0900

    feat: urls endpoints

---
+++ order-service/app/orders/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import CreateOrderView, OrderDetailView, UserOrdersView
+
+
+urlpatterns = [
+
+    path("orders/", CreateOrderView.as_view()),
+
+    path("orders/<uuid:order_id>/", OrderDetailView.as_view()),
+
+    path("orders/<uuid:user_id>/", UserOrdersView.as_view()),
+]
