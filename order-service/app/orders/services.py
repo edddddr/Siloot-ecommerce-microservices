df9b59e32cbf7c019196ecf5ccf526453a59dfb4
edddddr <endt6342@gmail.com>
2026-03-11 09:50:11 -0900

    feat: services layer create an order

---
+++ order-service/app/orders/services.py
@@ -0,0 +1,57 @@
+import uuid
+from decimal import Decimal
+from django.db import transaction
+
+from .models import Order, OrderItem, OrderStatus, OrderStatusHistory
+
+
+
+class OrderService:
+    """
+    Handles order business logic and orchestration.
+    """
+
+
+    @staticmethod
+    @transaction.atomic
+    def create_order(user_id, cart_items):
+        """
+        Create order and order items from cart data.
+        """
+
+        total_amount = Decimal("0.00")
+
+        order = Order.objects.create(
+            user_id=user_id,
+            status=OrderStatus.CREATED,
+            total_amount=Decimal("0.00")
+        )
+
+        for item in cart_items:
+
+            product_id = item["product_id"]
+            quantity = item["quantity"]
+            price = Decimal(item["price"])
+
+            OrderItem.objects.create(
+                order=order,
+                product_id=product_id,
+                quantity=quantity,
+                price=price
+            )
+
+            total_amount += price * quantity
+
+        order.total_amount = total_amount
+        order.save()
+
+        OrderStatusHistory.objects.create(
+            order=order,
+            status=OrderStatus.CREATED,
+            note="Order created from cart"
+        )
+
+        return order
+
+
+
