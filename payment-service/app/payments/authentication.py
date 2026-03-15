edeb59131c359e97efddfd9ef0b34c65e1aded59
edddddr <endt6342@gmail.com>
2026-03-15 07:10:25 -0900

    feat: internal service authenticator

---
+++ payment-service/app/payments/authentication.py
@@ -0,0 +1,59 @@
+import jwt
+
+from django.conf import settings
+from rest_framework.authentication import BaseAuthentication
+from rest_framework.exceptions import AuthenticationFailed
+
+
+class InternalServiceUser:
+    """
+    Fake user object for internal service authentication.
+    DRF requires request.user to exist and be authenticated.
+    """
+
+    is_authenticated = True
+
+    def __str__(self):
+        return "InternalServiceUser"
+
+
+class InternalServiceAuthentication(BaseAuthentication):
+
+    def authenticate(self, request):
+
+        auth_header = request.headers.get("Authorization")
+
+        if not auth_header:
+            raise AuthenticationFailed("Missing Authorization header")
+        
+        print(auth_header, "\n")
+
+        print("=======================",settings.JWT_PUBLIC_KEY)
+
+        parts = auth_header.split()
+
+        if len(parts) != 2 or parts[0].lower() != "bearer":
+            raise AuthenticationFailed("Invalid Authorization header")
+
+        token = parts[1]
+
+        try:
+            payload = jwt.decode(
+                token,
+                settings.JWT_PUBLIC_KEY,
+                algorithms=[settings.JWT_ALGORITHM],
+                options={"verify_aud": False},
+            )
+
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed("Token expired")
+
+        except jwt.InvalidTokenError:
+            raise AuthenticationFailed("Invalid token")
+
+        # ensure token is internal service token
+        if payload.get("role") != "internal_service":
+            raise AuthenticationFailed("Not an internal service token")
+
+        # return authenticated service user
+        return (InternalServiceUser(), payload)
